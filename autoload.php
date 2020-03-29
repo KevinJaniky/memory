a6cbb7b48c2286487bba7ffd3666df593fee56a3
kevinjaniky <kevin.janiky@gmail.com>
2020-03-29 16:47:37 +0200

    update config and remove game.php

--- autoload.php
+++ autoload.php
@@ -1,4 +1,7 @@
 <?php
+// on ajoute le fichier de config pour avoir acces au variable global de php lié au serveur
+require_once 'config.php';
+
 /* Il y a trois solutions pour autoloader nos classes PHP.
  * - Utiliser composer pour pouvoir bénéficier de PSR4 ( une norme PHP )
  * - Utiliser la fonction spl_autoload
@@ -6,6 +9,7 @@
  *
  * Dans notre cas nous allons utiliser la dernière méthode, car on a simplement 3 petites classes a ajouter.
  */
+
 
 require_once 'src/App/App.php';
 require_once 'src/Game/Game.php';
--- src/App/App.php
+++ src/App/App.php
@@ -1,13 +1,17 @@
 <?php
-class App {
+
+class App
+{
 
     protected $bdd;
 
     public function __construct()
     {
         try {
-            $this->bdd = new PDO('mysql:host=localhost;dbname=memory_oclock','root','');
-        }catch (Exception $e){
+            //Dans un cas réel, il est important de créer un dossier de conf et de ne pas giter les informations de la base de données
+            // Le fichier de conf sera gitignorer et permettra donc de modifier / ajouter les informations liés au serveur
+            $this->bdd = new PDO('mysql:host=' . HOST . ';dbname=' . DBNAME, USER, PASSWORD);
+        } catch (Exception $e) {
             echo $e->getMessage();
             exit();
         }
